06067a8a71ba081ec5b04bfcb5af4e74b769eb76
VseZnal <Vseznal66@gmail.com>
2023-03-05 18:48:26 +0300

    chore: config

--- backend/cmd/server/config/config.go
+++ backend/cmd/server/config/config.go
@@ -1,20 +1,23 @@
 package config
 
-import "testKwd/backend/libs/utils"
+import "testKwd/libs/utils"
 
 var (
-	serverHostEnvName = "SERVER_HOST"
-	serverPortEnvName = "SERVER_PORT"
+	serverHostEnvName   = "SERVER_HOST"
+	serverPortEnvName   = "SERVER_PORT"
+	pgConnStringEnvName = "PG_CONNECTION_STRING"
 )
 
 type Config struct {
-	HostServer string
-	PortServer string
+	HostServer   string
+	PortServer   string
+	PgConnString string
 }
 
 func ServerConfig() *Config {
 	return &Config{
-		HostServer: utils.TrimEnv(serverHostEnvName),
-		PortServer: utils.TrimEnv(serverPortEnvName),
+		HostServer:   utils.TrimEnv(serverHostEnvName),
+		PortServer:   utils.TrimEnv(serverPortEnvName),
+		PgConnString: utils.TrimEnv(pgConnStringEnvName),
 	}
 }
